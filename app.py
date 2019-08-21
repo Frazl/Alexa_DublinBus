031938b31253a72646604e86bbb31da22e9673d8
Fradl <sean.fradl@sap.com>
2019-08-21 09:35:33 +0100

    init

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template
+from flask_ask import Ask, statement, question, session
+import json
+import requests 
+import time 
+import unidecode
+
+app = Flask(__name__)
+ask = Ask(app, "/")
+
+def get_stop_information(stopNumber):
+    request = requests.get("https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid="+str(stopNumber)+"&format=json")
+    response = json.loads(request.content.decode('utf-8'))
+    bus_count = len(response['results'])
+    if bus_count == 1:
+        s = "There is {:n} bus coming...".format(bus_count)
+    elif bus_count < 1:
+        s = "There are no busses scheduled to arrive at this stop..."
+        return s
+    else:
+        s = "There are {:n} buses coming...".format(bus_count)
+    for i in range(len(response['results'])):
+        s += " There is a {:s} bus coming in {:s} minutes...".format(response['results'][i]['route'], response['results'][i]['duetime'])
+    return s
+
+
+@ask.launch 
+def start_skill():
+    welcome_message = 'What stop would you like information about?'
+    return question(welcome_message)
+
+@ask.intent("StopNumber", mapping={'stopNo': 'Stop'})
+def share_stop_number_informatoin(stopNo):
+    print(stopNo)
+    information = get_stop_information(1191)
+    return statement(information)
+
+@ask.intent("NegativeIntent")
+def negative_intent():
+    bye_text = "Rude for asking to me wake up then... I was having a nice nap... bye"
+    return statement(bye_text)
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)
